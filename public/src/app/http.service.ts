9b46c4f07bbd57b3e6975b21126914588b42288c
Ismel Salazar <ismel.salazar@gmail.com>
2023-07-04 22:29:12 -0400

    Adds environment-specific api base URL to http service

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +12,18 @@ export class HttpService {
   constructor(private _http: HttpClient) { }
 
   getAllPets() {
-    return this._http.get('/api/pets');
+    return this._http.get(`${environment.apiBaseURL}/pets`);
   }
   getPet(petId) {
-    return this._http.get(`/api/pets/${petId}`);
+    return this._http.get(`${environment.apiBaseURL}/pets/${petId}`);
   }
   createPet(newPet) {
-    return this._http.post(`/api/pets`, newPet);
+    return this._http.post(`${environment.apiBaseURL}/pets`, newPet);
   }
   editPet(editedPet) {
-    return this._http.put(`/api/pets/${editedPet._id}`, editedPet);
+    return this._http.put(`${environment.apiBaseURL}/pets/${editedPet._id}`, editedPet);
   }
   deletePet(petId) {
-    return this._http.delete(`/api/pets/${petId}`);
+    return this._http.delete(`${environment.apiBaseURL}/pets/${petId}`);
   }
 }
