6cc51739e57bdc94e3c0342ddd99a3fb26648f78
Ismel Salazar <ismel.salazar@gmail.com>
2023-09-07 15:08:44 -0400

    Updates mongo connection string.

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const routes = require('./Server/Routes/routes')
-const cors = require('cors')
+const mongoose = require("mongoose");
+const routes = require("./Server/Routes/routes");
+const cors = require("cors");
 
-app.use(express.static(__dirname + '/public/dist/public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public/dist/public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://localhost/beltExamPets', {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true});
+mongoose
+  .connect("mongodb://127.0.0.1/beltExamPets", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((value) => {
+    console.log("Connected to MongoDB");
+    routes(app);
 
-routes(app);
-
-const port = 8000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+    const port = 8000;
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
