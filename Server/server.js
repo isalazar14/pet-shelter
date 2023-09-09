8fe5b58d0f0a243faa375d9b6764efab2d05b915
Ismel Salazar <ismel.salazar@gmail.com>
2023-09-08 22:53:36 -0400

    Adds .env setup. Updates server to use .env.

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,13 @@
 const express = require("express"),
- app = express(),
- mongoose = require("mongoose"),
-//  routes = require("./Server/Routes/routes"),
- cors = require("cors"),
- petRouter = require("./routes/pets"),
- userRouter = require("./routes/users")
+  app = express(),
+  mongoose = require("mongoose"),
+  //  routes = require("./Server/Routes/routes"),
+  cors = require("cors"),
+  petRouter = require("./routes/pets"),
+  userRouter = require("./routes/users"),
+  path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +18,11 @@ app.use("/api/users/", userRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("../client/dist/public/index.html"));
 });
-app.all('*', (req, res) => {
-    res.sendStatus(500)
-})
+app.all("*", (req, res) => {
+  res.sendStatus(500);
+});
 mongoose
-  .connect("mongodb://127.0.0.1/beltExamPets", {
+  .connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -27,7 +30,7 @@ mongoose
   .then((value) => {
     console.log("Connected to MongoDB");
 
-    const port = 8000;
+    const port = process.env.SERVER_PORT;
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
